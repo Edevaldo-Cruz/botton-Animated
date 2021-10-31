96b40e1fe2c0ffe805b2d57a6b4eba6a14ad5643
Edevaldo-Cruz <77896103+Edevaldo-Cruz@users.noreply.github.com>
2021-10-31 19:14:12 -0300

    Desenvolvendo botão com animação usando animated.

--- App.js
+++ App.js
@@ -1,12 +1,193 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {
+  StatusBar,
+  Dimensions,
+  TouchableOpacity,
+  Animated,
+  View,
+  StyleSheet,
+} from "react-native";
+import Constants from "expo-constants";
+import { AntDesign } from "@expo/vector-icons";
+
+const { width } = Dimensions.get("window");
+
+const AnimatedAntDesign = Animated.createAnimatedComponent(AntDesign);
+
+const DURATION = 2500;
+
+const Circle = ({ onPress, index, animatedValue, animatedValue2 }) => {
+  const { initialBgColor, nextBgColor, bgColor } = colors[index];
+  const inputRange = [0, 0.001, 0.5, 0.501, 1];
+  const backgroundColor = animatedValue2.interpolate({
+    inputRange,
+    outputRange: [
+      initialBgColor,
+      initialBgColor,
+      initialBgColor,
+      bgColor,
+      bgColor,
+    ],
+  });
+  const dotBgColor = animatedValue2.interpolate({
+    inputRange: [0, 0.001, 0.5, 0.501, 0.9, 1],
+    outputRange: [
+      bgColor,
+      bgColor,
+      bgColor,
+      initialBgColor,
+      initialBgColor,
+      nextBgColor,
+    ],
+  });
+
+  return (
+    <Animated.View
+      style={[
+        StyleSheet.absoluteFillObject,
+        styles.container,
+        { backgroundColor },
+      ]}
+    >
+      <Animated.View
+        style={[
+          styles.circle,
+          {
+            backgroundColor: dotBgColor,
+            transform: [
+              { perspective: 250 },
+              {
+                rotateY: animatedValue2.interpolate({
+                  inputRange: [0, 0.2, 0.4, 0.6, 1],
+                  outputRange: ["0deg", "90deg", "0deg", "-90deg", "-180deg"],
+                }),
+              },
+
+              {
+                scale: animatedValue2.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: [1, 6, 1],
+                }),
+              },
+
+              {
+                translateX: animatedValue2.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: [0, 0.5, 0],
+                }),
+              },
+            ],
+          },
+        ]}
+      >
+        <TouchableOpacity onPress={onPress}>
+          <Animated.View
+            style={[
+              styles.button,
+              {
+                transform: [
+                  {
+                    scale: animatedValue.interpolate({
+                      inputRange: [0, 0.05, 0.5, 1],
+                      outputRange: [1, 0, 0, 1],
+                    }),
+                  },
+                  {
+                    rotateY: animatedValue.interpolate({
+                      inputRange: [0, 0.5, 0.9, 1],
+                      outputRange: ["0deg", "180deg", "180deg", "180deg"],
+                    }),
+                  },
+                ],
+                opacity: animatedValue.interpolate({
+                  inputRange: [0, 0.05, 0.9, 1],
+                  outputRange: [1, 0, 0, 1],
+                }),
+              },
+            ]}
+          >
+            <AnimatedAntDesign name="arrowright" size={28} color={"white"} />
+          </Animated.View>
+        </TouchableOpacity>
+      </Animated.View>
+    </Animated.View>
+  );
+};
+const colors = [
+  {
+    initialBgColor: "#2D2424",
+    bgColor: "#5C3D2E",
+    nextBgColor: "#5C3D2E",
+  },
+  {
+    initialBgColor: "#2D2424",
+    bgColor: "#5C3D2E",
+    nextBgColor: "#B85C38",
+  },
+  {
+    initialBgColor: "#5C3D2E",
+    bgColor: "#B85C38",
+    nextBgColor: "#ffc0cb",
+  },
+  {
+    initialBgColor: "#B85C38",
+    bgColor: "#ffc0cb",
+    nextBgColor: "#23074d",
+  },
+  {
+    initialBgColor: "#ffc0cb",
+    bgColor: "#23074d",
+    nextBgColor: "#2D2424",
+  },
+  {
+    initialBgColor: "#23074d",
+    bgColor: "#2D2424",
+    nextBgColor: "#5C3D2E",
+  },
+];
 
 export default function App() {
+  const animatedValue = React.useRef(new Animated.Value(0)).current;
+  const animatedValue2 = React.useRef(new Animated.Value(0)).current;
+  const sliderAnimatedValue = React.useRef(new Animated.Value(0)).current;
+
+  const [index, setIndex] = React.useState(0);
+
+  const animate = (i) =>
+    Animated.parallel([
+      Animated.timing(sliderAnimatedValue, {
+        toValue: i,
+
+        useNativeDriver: true,
+      }),
+      Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: DURATION,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animatedValue2, {
+        toValue: 1,
+        duration: DURATION,
+        useNativeDriver: false,
+      }),
+    ]);
+
+  const onPress = () => {
+    animatedValue.setValue(0);
+    animatedValue2.setValue(0);
+    animate((index + 1) % colors.length).start();
+    setIndex((index + 1) % colors.length);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "flex-start", paddingTop: 100 }}>
+      <StatusBar hidden />
+      <Circle
+        index={index}
+        onPress={onPress}
+        animatedValue={animatedValue}
+        animatedValue2={animatedValue2}
+      />
     </View>
   );
 }
@@ -14,8 +195,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+    padding: 8,
+    paddingBottom: 50,
+  },
+  button: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  circle: {
+    backgroundColor: "#23074d",
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
 });
